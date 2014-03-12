4729cd1b433d72ddd24bdf1071ed9d88dd981ea1
Matt George <mattgeorge1@gmail.com>
2014-03-12 13:39:58 -0700

    add selectData function to make basic select queries
    Includes helper functions to get table and column names. Moved lines
    to open sqlite db into their own method. Working towards #2.
    
    selectData takes a few values to slice on, queries the sqlite db and
    returns a numpy recarray. Some of these features should be split up
    into separate functions, and better tests are needed.

--- BusTripper/io.py
+++ BusTripper/io.py
@@ -1,5 +1,10 @@
-import psycopg2
+import types
+from collections import Iterable
+import re
+import numpy as np
 import sqlite3
+import psycopg2
+
 
 def readKeys(filename):
     """Construct dict with keys from file
@@ -21,8 +26,7 @@ def readKeys(filename):
 def createLocalDB(localDBFilename):
     """Make a local sqlite database with same format as remote PostgreSQL"""
 
-    lcon = sqlite3.connect(localDBFilename)
-    lcur = lcon.cursor()
+    lcon, lcur = openLocalDB(localDBFilename)
     lcur.executescript("""
         CREATE TABLE event_subset
         (
@@ -64,7 +68,7 @@ def createLocalDB(localDBFilename):
     lcon.close()
 
 def copyRemoteRows(lcur, rcur, limit = 10):
-    """Copy enties from remote db to local db
+    """Copy entries from remote db to local db
 
     Inputs:
         lcur - sqlite cursor for local db
@@ -99,8 +103,7 @@ def copyRemoteDB(loginFilename, localDBFilename, limit = 10):
 
     # Make local DB
     createLocalDB(localDBFilename)
-    lcon = sqlite3.connect(localDBFilename)
-    lcur = lcon.cursor()
+    lcon, lcur = openLocalDB(localDBFilename)
 
     # Open remote DB
     loginDict = readKeys(loginFilename)
@@ -113,3 +116,115 @@ def copyRemoteDB(loginFilename, localDBFilename, limit = 10):
     # Save changes and close
     lcon.commit()
     [xx.close() for xx in lcur, lcon, rcur, rcon]
+
+def openLocalDB(localDBFilename):
+    """Open connection and cursor to SQLite database"""
+    con = sqlite3.connect(localDBFilename)
+    con.row_factory = sqlite3.Row # allows indexing rows by column names
+    cur = con.cursor()
+    return (con, cur)
+
+def getTableNames(cur):
+    cur.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
+    result = cur.fetchall()
+    return [line[0] for line in result]
+
+def getColumnNames(cur, tableName):
+    if tableName not in getTableNames(cur):
+        raise ValueError(tableName)
+
+    cur.execute("PRAGMA TABLE_INFO({})".format(tableName))
+    result = cur.fetchall()
+    return [line[1] for line in result]
+
+def selectData(cur, cols=None, tableName="raw_loc_subset", date=None,
+               time=None, deviceID=None, limit=None):
+    """Construct and execute select query
+
+    SELECT {cols} FROM {tableName} [WHERE {date, time, deviceID}]
+    """
+
+    # Get string of selected column names for query
+    columns = getColumnNames(cur, tableName)  # full list to check against
+
+    if cols is None:  # no restriction on cols, select all
+        qCols = "*"
+        cols = columns
+    elif (cols in columns) or (cols == '*'):  # single string
+        qCols = cols
+        cols = columns
+    elif isinstance(cols, Iterable):  # list of columns
+        qCols = []
+        for col in cols:
+            if col in columns:
+                qCols.append(col)
+            else:
+                raise ValueError(col)
+        qCols = ','.join(qCols)
+    else:
+        raise ValueError(col)
+
+
+    # Get selection (where clauses)
+    qWhere = []
+    dateFmt = "^[0-9]{4}-[0-9]{2}-[0-9]{2}$" # regex for YYYY-MM-DD
+    timeFmt = "^[0-9]{2}:[0-9]{2}:[0-9]{2}$" # regex for HH:MM:SS
+
+    if date is not None:
+        if re.match(dateFmt, date):
+            qWhere.append("DATE(time/1000, 'UNIXEPOCH') = {}".format(date))
+        elif isinstance(date, Iterable): # range
+            if re.match(dateFmt, date[0]) and re.match(dateFmt, date[1]):
+                qWhere.append("DATE(time/1000,'UNIXEPOCH') >= {}".format(
+                    date[0]))
+                qWhere.append("DATE(time/1000, 'UNIXEPOCH') <= {}".format(
+                    date[1]))
+            else:
+                raise ValueError(date)
+        else:
+            raise ValueError(date)
+
+    if time is not None:
+        if re.match(timeFmt, time):
+            qWhere.append("TIME(time/1000, 'UNIXEPOCH') = {}".format(time))
+        elif isinstance(time, Iterable): # range
+            if re.match(timeFmt, time[0]) and re.match(timeFmt, time[1]):
+                qWhere.append("TIME(time/1000,'UNIXEPOCH') >= {}".format(
+                    time[0]))
+                qWhere.append("TIME(time/1000, 'UNIXEPOCH') <= {}".format(
+                    time[1]))
+            else:
+                raise ValueError(time)
+        else:
+            raise ValueError(time)
+
+    if deviceID is not None:
+        if isinstance(deviceID, types.StringTypes):
+            qWhere.append("device_id = {}".format(deviceID))
+        elif isinstance(deviceID, Iterable):
+            qWhere.append("device_id IN {}".format(','.join(deviceID)))
+        else:
+            raise ValueError(deviceID)
+
+    if len(qWhere) > 0:
+        qWhere = "WHERE " + ' AND '.join(qWhere)
+
+    # Limit clause
+    if limit is None:
+        qLim = ""
+    else:
+        qLim = "LIMIT {}".format(limit)
+
+    # Combine clauses and execute query
+    query = "SELECT {} FROM {} {} {}".format(qCols, tableName, qWhere, qLim)
+    cur.execute(query)
+    result = cur.fetchall()
+
+    # Translate into numpy recarray
+    data = [np.array([row[str(col)] for row in result]) for col in cols]
+    fmt = [arr.dtype.str for arr in data]
+    rec = np.recarray(data[0].size, formats = fmt, names = cols)
+    for colNum,col in enumerate(cols):
+        rec[col] = data[colNum]
+
+    return rec

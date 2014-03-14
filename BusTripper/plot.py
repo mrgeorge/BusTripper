eea2baaeee184ba57c122786bc107451ae2c0348
Matt George <mattgeorge1@gmail.com>
2014-03-14 13:34:37 -0700

    refactor and clean plot example
    now takes kwargs for date and deviceID. speed and time color labels
    are handled with a kwarg instead of repeating code. new showDepot and
    getDepotCoords functions overlay a circle where the depot is for
    orientation.

--- BusTripper/plot.py
+++ BusTripper/plot.py
@@ -3,35 +3,55 @@ import numpy as np
 
 import io
 
-def example(cur, plotDir):
+def getDepotCoords():
+    return (-1.96143, 43.3172) # lon, lat
+
+def showDepot():
+    circ = plt.Circle(getDepotCoords(), radius=0.001, color='gray',
+                      fill=False, lw=3, ls='dashed')
+    ax = plt.gca()
+    ax.add_patch(circ)
+
+def plotDeviceDate(cur, date='2013-08-01', deviceID='c08080a19e816bf',
+            plotFilename=None, showPlot=False, colorType="time"):
     """Plot GPS stream over one day for a single device
 
     Inputs:
         cur - cursor to SQLite db (dbus_events.db)
-        plotDir - directory to save plot files
+        date - YYYY-MM-DD format for date selection
+        deviceID - string
+        plotFilename - string with path to save plot files
+        showPlot - boolean (True) for interactive plot
+        colorType - "time" or "speed" used to color points
     Outputs:
-        plots of GPS stream saved to plotDir
+        plots of GPS stream displayed or saved
     """
 
-    rec = io.selectData(cur, date = '2013-08-01', deviceID='c08080a19e816bf')
+    rec = io.selectData(cur, date=date, deviceID=deviceID)
 
-    # color = time
+    if colorType == "time":
+        cArr = (rec.time-np.min(rec.time))/1000./3600
+        cLabel = "Hours since start of day"
+    elif colorType == "speed":
+        cArr = rec.speed
+        cLabel = "Speed (m/s)"
+    else:
+        raise ValueError(colorType)
+
     plt.clf()
     plt.scatter(rec.longitude, rec.latitude,
-                c=(rec.time-np.min(rec.time))/1000./3600, s=10, linewidth=0,
+                c=cArr, s=10, linewidth=0,
                 alpha=0.5)
     cbar = plt.colorbar()
-    cbar.set_label("hours since start of day")
-    plt.xlabel('longitude')
-    plt.ylabel('latitude')
-    plt.savefig(plotDir + "fig1.png")
+    cbar.set_label(cLabel)
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.title("{}   {}".format(date,deviceID))
 
-    # color = speed
-    plt.clf()
-    plt.scatter(rec.longitude, rec.latitude, c=rec.speed, s=10, linewidth=0,
-                alpha=0.5)
-    cbar = plt.colorbar()
-    cbar.set_label("speed")
-    plt.xlabel('longitude')
-    plt.ylabel('latitude')
-    plt.savefig(plotDir + "fig2.png")
+    showDepot()
+
+    if showPlot:
+        plt.show()
+    if plotFilename is not None:
+        plt.savefig(plotFilename)
+
